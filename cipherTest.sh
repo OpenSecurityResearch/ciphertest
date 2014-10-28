ea5ceeeac7b4601402bad6c83eddd60a0e20547e
Patrick Bogen <pbogen@twitter.com>
2014-10-28 15:37:06 -0700

    Licensed under GNU GPL v3.0

--- cipherTest.sh
+++ cipherTest.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+# Copyright 2014 Patrick Bogen
+#
+# This file is part of ciphertest.
+#
+# ciphertest is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ciphertest is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ciphertest.  If not, see <http://www.gnu.org/licenses/>.
+
 control_c() {
 	[ -t 1 ] && echo "\r\e[K"
 	exit 1
